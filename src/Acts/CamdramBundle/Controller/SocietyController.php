6e2116065d1e58a41562dc42ad752141c5c33218
Andrew Featherstone <andrew.featherstone@gmail.com>
2012-12-16 14:29:28 +0000

    Chasing my own tail.

--- src/Acts/CamdramBundle/Controller/SocietyController.php
+++ src/Acts/CamdramBundle/Controller/SocietyController.php
@@ -16,15 +16,9 @@ class SocietyController extends FOSRestController
     public function getAction($id)
     {
         $repo = $this->getDoctrine()->getEntityManager()->getRepository('ActsCamdramBundle:Society');
-<<<<<<< HEAD
         $society = $repo->findOneById($id);
         
         $view = $this->view($society, 200)
-=======
-        $show = $repo->findOneById($id);
-        
-        $view = $this->view($show, 200)
->>>>>>> d85b8092a83851489d0ef21e548f352ee862298a
             ->setTemplate("ActsCamdramBundle:Society:index.html.twig")
             ->setTemplateVar('society')
         ;
