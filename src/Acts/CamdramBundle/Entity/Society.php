ffe32aa8b31c47da0b7743af54fe7e6cdf13e717
Peter Hoyes <pahoyes@gmail.com>
2012-12-19 23:43:40 +0000

    Fixed society page. Problem was that both 'Organisation' and 'Society' had a $short_name field and getShortName() method, and Doctrine was populating the one in 'Organisation', was is overridden by the one in Society. In other words, I'd done the inheritance bit but not removed the redundant rows from the two classes. #1

--- src/Acts/CamdramBundle/Entity/Society.php
+++ src/Acts/CamdramBundle/Entity/Society.php
@@ -12,47 +12,6 @@ use Doctrine\ORM\Mapping as ORM;
  */
 class Society extends Organisation
 {
-    /**
-     * @var string
-     *
-     * @ORM\Column(name="shortname", type="string", length=255, nullable=false)
-     */
-    private $short_name;
-
-    /**
-     * @var string
-     *
-     * @ORM\Column(name="college", type="string", length=255, nullable=true)
-     */
-    private $college;
-
-    /**
-     * @var boolean
-     *
-     * @ORM\Column(name="type", type="boolean", nullable=false)
-     */
-    private $type;
-
-    /**
-     * @var boolean
-     *
-     * @ORM\Column(name="affiliate", type="boolean", nullable=false)
-     */
-    private $affiliate;
-
-    /**
-     * @var string
-     *
-     * @ORM\Column(name="logourl", type="text", nullable=true)
-     */
-    private $logo_url;
-
-    /**
-     * @var \DateTime
-     *
-     * @ORM\Column(name="expires", type="date", nullable=false)
-     */
-    private $expires;
 
     /**
      * @ORM\OneToMany(targetEntity="Show", mappedBy="society")
@@ -60,150 +19,13 @@ class Society extends Organisation
     private $shows;
 
     /**
-     * Set short_name
-     *
-     * @param string $shortName
-     * @return Society
-     */
-    public function setShortName($shortName)
-    {
-        $this->short_name = $shortName;
-    
-        return $this;
-    }
-
-    /**
-     * Get short_name
-     *
-     * @return string 
-     */
-    public function getShortName()
-    {
-        return $this->short_name;
-    }
-
-    /**
-     * Set college
-     *
-     * @param string $college
-     * @return Society
-     */
-    public function setCollege($college)
-    {
-        $this->college = $college;
-    
-        return $this;
-    }
-
-    /**
-     * Get college
-     *
-     * @return string 
-     */
-    public function getCollege()
-    {
-        return $this->college;
-    }
-
-    /**
-     * Set type
-     *
-     * @param boolean $type
-     * @return Society
-     */
-    public function setType($type)
-    {
-        $this->type = $type;
-    
-        return $this;
-    }
-
-    /**
-     * Get type
-     *
-     * @return boolean 
-     */
-    public function getType()
-    {
-        return $this->type;
-    }
-
-    /**
-     * Set affiliate
-     *
-     * @param boolean $affiliate
-     * @return Society
-     */
-    public function setAffiliate($affiliate)
-    {
-        $this->affiliate = $affiliate;
-    
-        return $this;
-    }
-
-    /**
-     * Get affiliate
-     *
-     * @return boolean 
-     */
-    public function getAffiliate()
-    {
-        return $this->affiliate;
-    }
-
-    /**
-     * Set logo_url
-     *
-     * @param string $logoUrl
-     * @return Society
-     */
-    public function setLogoUrl($logoUrl)
-    {
-        $this->logo_url = $logoUrl;
-    
-        return $this;
-    }
-
-    /**
-     * Get logo_url
-     *
-     * @return string 
-     */
-    public function getLogoUrl()
-    {
-        return $this->logo_url;
-    }
-
-    /**
-     * Set expires
-     *
-     * @param \DateTime $expires
-     * @return Society
-     */
-    public function setExpires($expires)
-    {
-        $this->expires = $expires;
-    
-        return $this;
-    }
-
-    /**
-     * Get expires
-     *
-     * @return \DateTime 
-     */
-    public function getExpires()
-    {
-        return $this->expires;
-    }
-    /**
      * Constructor
      */
     public function __construct()
     {
         $this->shows = new \Doctrine\Common\Collections\ArrayCollection();
     }
-    
+
     /**
      * Add shows
      *
@@ -213,7 +35,7 @@ class Society extends Organisation
     public function addShow(\Acts\CamdramBundle\Entity\Show $shows)
     {
         $this->shows[] = $shows;
-    
+
         return $this;
     }
 
@@ -230,7 +52,7 @@ class Society extends Organisation
     /**
      * Get shows
      *
-     * @return \Doctrine\Common\Collections\Collection 
+     * @return \Doctrine\Common\Collections\Collection
      */
     public function getShows()
     {
