0dfc8013f6e45b7e1ddbd5e3c34e4e49f12d6f66
Stuart Moore <stumo@stumo.org.uk>
2014-02-24 13:45:54 +0000

    Improving handling of society-only applications
    Fixing issue introduced by last merge with audextra

--- src/Acts/CamdramBackendBundle/DataFixtures/ORM/SocietyFixtures.php
+++ src/Acts/CamdramBackendBundle/DataFixtures/ORM/SocietyFixtures.php
@@ -5,6 +5,7 @@ use Doctrine\Common\Persistence\ObjectManager;
 use Doctrine\Common\DataFixtures\OrderedFixtureInterface;
 use Doctrine\Common\DataFixtures\AbstractFixture;
 use Acts\CamdramBundle\Entity\Society;
+use Acts\CamdramBundle\Entity\Application;
 
 class SocietyFixtures extends AbstractFixture implements OrderedFixtureInterface
 {
@@ -19,6 +20,11 @@ class SocietyFixtures extends AbstractFixture implements OrderedFixtureInterface
             $society->setShortName("S-$i");
             $society->setDescription("Description of society $i");
             $manager->persist($society);
+            
+            if(mt_rand(0,1)==0){
+                $this->addApplication($manager, $society);
+            }
+            
         }
         $manager->flush();
     }
@@ -29,5 +35,16 @@ class SocietyFixtures extends AbstractFixture implements OrderedFixtureInterface
     public function getOrder()
     {
         return 1;
+    }
+ 
+    private function addApplication(ObjectManager $manager, Society $society)
+    {
+        $application = new Application();
+        $application->setText("Random text " . mt_rand(1,100));
+        $application->setDeadlineDate(new \DateTime(mt_rand(-5,15) . " days"));
+        $application->setFurtherInfo("Further Info text " . mt_rand(1,100));
+        $application->setDeadlineTime(new \DateTime(mt_rand(0,23) . ":" . mt_rand(0,3) * 15));
+        $application->setSociety($society);       
+        $manager->persist($application);   
     }
 }
