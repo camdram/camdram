87499f13961f0bc9f084c30891ad46f48516d17e
Andrew Featherstone <andrew.featherstone@gmail.com>
2014-02-25 00:12:42 +0000

    Promote pending ACEs from acts_pendingaccess to acts_access on User creation.
    Work as part of #76 and #77: this is common for access tokens.

--- src/Acts/CamdramSecurityBundle/EventListener/UserListener.php
+++ src/Acts/CamdramSecurityBundle/EventListener/UserListener.php
@@ -2,22 +2,52 @@
 namespace Acts\CamdramSecurityBundle\EventListener;
 
 
-use Acts\CamdramSecurityBundle\Entity\User;
+use Acts\CamdramSecurityBundle\Entity\AccessControlEntry,
+    Acts\CamdramSecurityBundle\Entity\User;
 use Acts\CamdramSecurityBundle\Service\EmailDispatcher;
 use Acts\CamdramSecurityBundle\Service\TokenGenerator;
-use Doctrine\ORM\Event\LifecycleEventArgs;
-use Doctrine\ORM\Event\PreUpdateEventArgs;
 
-class UserListener {
+use Doctrine\ORM\EntityManager,
+    Doctrine\ORM\Event\LifecycleEventArgs,
+    Doctrine\ORM\Event\PreUpdateEventArgs;
 
+class UserListener 
+{
+    /**
+     * @var \Doctrine\ORM\EntityManager
+     */
+    private $entityManager;
     private $emailDispatcher;
-
     private $tokenGenerator;
 
-    public function __construct(EmailDispatcher $emailDispatcher,  TokenGenerator $tokenGenerator)
+    public function __construct(EntityManager $em, EmailDispatcher $emailDispatcher,  TokenGenerator $tokenGenerator)
     {
+        $this->entityManager = $em;
         $this->emailDispatcher = $emailDispatcher;
         $this->tokenGenerator = $tokenGenerator;
+    }
+
+    /**
+     * Delete any pending access tokens given to this user, and grant access to 
+     * those resources in turn.
+     */
+    public function postPersist(User $user, LifecycleEventArgs $event)
+    {
+        $pending_aces = $this->entityManager->getRepository('ActsCamdramSecurityBundle:PendingAccess')
+                            ->findByEmail($user->getEmail());
+        foreach ($pending_aces as $pending) {
+            $ace = new AccessControlEntry();
+            $ace->setUser($user)
+                ->setEntityId($pending->getRid())
+                ->setCreatedAt(new \DateTime)
+                ->setGrantedBy($pending->getIssuer())
+                ->setGrantedAt(new \DateTime)
+                ->setType($pending->getType());
+
+            $this->entityManager->persist($ace);
+            $this->entityManager->remove($pending);
+        }
+        $this->entityManager->flush();
     }
 
     public function preUpdate(User $user, PreUpdateEventArgs $event)
@@ -27,5 +57,5 @@ class UserListener {
             $this->emailDispatcher->sendEmailVerifyEmail($user, $token);
         }
     }
+}
 
-} 
