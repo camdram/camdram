406f549582b831d1631dd05e36b2b8f389bf7979
Peter Hoyes <pahoyes@gmail.com>
2015-02-23 22:16:10 +0000

    Forced recompute of changeset in show entity listener #285

--- src/Acts/CamdramBundle/EventListener/ShowListener.php
+++ src/Acts/CamdramBundle/EventListener/ShowListener.php
@@ -5,6 +5,8 @@ use Acts\CamdramBundle\Entity\Show;
 use Acts\CamdramBundle\Service\ModerationManager;
 use Doctrine\Common\Persistence\Event\LifecycleEventArgs;
 use Doctrine\Common\Persistence\ObjectManager;
+use Doctrine\ORM\EntityManager;
+use Doctrine\ORM\Event\PreUpdateEventArgs;
 
 class ShowListener
 {
@@ -15,18 +17,23 @@ class ShowListener
         $this->moderationManager = $moderationManager;
     }
 
-    private function updateFields(Show $show, ObjectManager $om)
+    private function updateFields(Show $show, EntityManager $om)
     {
+        //ensure the venue attached to the show and to the performances are consistent
+        $this->updateVenues($show);
+        //ensure the start_at and end_at fields are equal to the start and end of the first and last performances
+        $this->updateTimes($show);
+
+        $uow  = $om->getUnitOfWork();
+        $meta = $om->getClassMetadata(get_class($show));
+        $uow->recomputeSingleEntityChangeSet($meta, $show);
+
         //ensure all the associated performances are also saved
+        $performanceMeta = $om->getClassMetadata('ActsCamdramBundle:Performance');
         foreach ($show->getPerformances() as $performance) {
             $performance->setShow($show);
-            $om->persist($performance);
+            $uow->recomputeSingleEntityChangeSet($performanceMeta, $performance);
         }
-
-        //ensure the venue attached to the show and to the performances are consistent
-        $show->updateVenues();
-        //ensure the start_at and end_at fields are equal to the start and end of the first and last performances
-        $show->updateTimes();
     }
 
     public function prePersist(Show $show, LifecycleEventArgs $event)
@@ -39,8 +46,61 @@ class ShowListener
         $this->moderationManager->autoApproveOrEmailModerators($show);
     }
 
-    public function preUpdate(Show $show, LifecycleEventArgs $event)
+    public function preUpdate(Show $show, PreUpdateEventArgs $event)
     {
         $this->updateFields($show, $event->getObjectManager());
+    }
+
+    private function updateVenues(Show $show)
+    {
+        switch ($show->getMultiVenue()) {
+            case 'single':
+                foreach ($show->getPerformances() as $performance) {
+                    $performance->setVenue($show->getVenue());
+                    $performance->setVenueName($show->getVenueName());
+                }
+                break;
+            case 'multi':
+                //Try to work out the 'main' venue
+                //First count venue objects and venue names
+                $venues = array();
+                $venue_counts = array();
+                $name_counts = array();
+                foreach ($show->getPerformances() as $performance) {
+                    if ($performance->getVenue()) {
+                        $key = $performance->getVenue()->getId();
+                        if (!isset($venue_counts[$key])) $venue_counts[$key] = 1;
+                        else $venue_counts[$key]++;
+                        $venues[$key] = $performance->getVenue();
+                    }
+                    if ($performance->getVenueName()) {
+                        $key = $performance->getVenueName();
+                        if (!isset($name_counts[$key])) $name_counts[$key] = 1;
+                        else $name_counts[$key]++;
+                    }
+                    //Favour a venue object over a venue name
+                    if (count($venue_counts) > 0) {
+                        $venue_id = array_search(max($venue_counts), $venue_counts);
+                        $show->setVenue($venues[$venue_id]);
+                    } else {
+                        $venue_name = array_search(max($name_counts), $name_counts);
+                        $show->setVenueName($venue_name);
+                    }
+                }
+                break;
+        }
+    }
+
+
+    public function updateTimes(Show $show)
+    {
+        $min = null;
+        $max = null;
+        foreach ($show->getPerformances() as $performance) {
+            if (is_null($min) || $performance->getStartDate() < $min) $min = $performance->getStartDate();
+            if (is_null($max) || $performance->getEndDate() > $max) $max = $performance->getEndDate();
+        }
+        $show->setStartAt($min);
+        $show->setEndAt($max);
     }
 }
