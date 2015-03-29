f91dbf47c38d9ced3516400ccf079dc30cf9b183
Peter Hoyes <pahoyes@gmail.com>
2015-03-29 21:29:40 +0100

    symfony/symfony#13450 means tests currently broken. Hacked around it by swapping out test session class. Also updated PHP deps

---
+++ src/Acts/CamdramBackendBundle/Test/SessionStorage.php
@@ -0,0 +1,17 @@
+<?php
+namespace Acts\CamdramBackendBundle\Test;
+
+use Symfony\Component\HttpFoundation\Session\Storage\MockFileSessionStorage;
+
+class SessionStorage extends MockFileSessionStorage
+{
+    /**
+     * Override setId. For some reason it gets called in the wrong order during tests, causing an exception
+     *
+     * @param string $id
+     */
+    public function setId($id)
+    {
+        $this->id = $id;
+    }
+}
