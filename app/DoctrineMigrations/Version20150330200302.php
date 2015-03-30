9ddff9205529733c4b0ba7008a84c0301948ada8
Peter Hoyes <pahoyes@gmail.com>
2015-03-30 20:08:18 +0100

    Made role adding obey mapto field #282

--- app/DoctrineMigrations/Version20150330200302.php
+++ app/DoctrineMigrations/Version20150330200302.php
@@ -8,7 +8,7 @@ use Doctrine\DBAL\Schema\Schema;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-class Version20150330003551 extends AbstractMigration
+class Version20150330200302 extends AbstractMigration
 {
     /**
      * @param Schema $schema
@@ -18,7 +18,10 @@ class Version20150330003551 extends AbstractMigration
         // this up() migration is auto-generated, please modify it to your needs
         $this->abortIf($this->connection->getDatabasePlatform()->getName() != 'mysql', 'Migration can only be executed safely on \'mysql\'.');
 
-        $this->addSql('CREATE TABLE acts_email_bounces (id INT AUTO_INCREMENT NOT NULL, from_header VARCHAR(255) NOT NULL, to_header VARCHAR(255) NOT NULL, subject VARCHAR(255) NOT NULL, body LONGTEXT NOT NULL, received_at DATETIME NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci ENGINE = InnoDB');
+        $this->addSql('ALTER TABLE acts_people_data CHANGE mapto mapto INT DEFAULT NULL');
+        $this->addSql('UPDATe acts_people_data p left join acts_people_data m ON m.id = p.mapto SET p.mapto = NULL WHERE m.id IS NULL');
+        $this->addSql('ALTER TABLE acts_people_data ADD CONSTRAINT FK_567E1F8FE6B57CEC FOREIGN KEY (mapto) REFERENCES acts_people_data (id)');
+        $this->addSql('CREATE INDEX IDX_567E1F8FE6B57CEC ON acts_people_data (mapto)');
     }
 
     /**
@@ -29,6 +32,8 @@ class Version20150330003551 extends AbstractMigration
         // this down() migration is auto-generated, please modify it to your needs
         $this->abortIf($this->connection->getDatabasePlatform()->getName() != 'mysql', 'Migration can only be executed safely on \'mysql\'.');
 
-        $this->addSql('DROP TABLE acts_email_bounces');
+        $this->addSql('ALTER TABLE acts_people_data DROP FOREIGN KEY FK_567E1F8FE6B57CEC');
+        $this->addSql('DROP INDEX IDX_567E1F8FE6B57CEC ON acts_people_data');
+        $this->addSql('ALTER TABLE acts_people_data CHANGE mapto mapto INT NOT NULL');
     }
 }
