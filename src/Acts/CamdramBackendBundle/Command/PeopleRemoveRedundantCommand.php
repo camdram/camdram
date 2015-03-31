396b349c190c8a2861b6bea4613d67cfd778b4d0
Peter Hoyes <pahoyes@gmail.com>
2015-03-31 21:45:45 +0100

    Made 'remove redundant person' command deal correctly with mapped people #195

--- src/Acts/CamdramBackendBundle/Command/PeopleRemoveRedundantCommand.php
+++ src/Acts/CamdramBackendBundle/Command/PeopleRemoveRedundantCommand.php
@@ -1,6 +1,7 @@
 <?php
 namespace Acts\CamdramBackendBundle\Command;
 
+use Acts\CamdramBundle\Entity\Person;
 use Symfony\Bundle\FrameworkBundle\Command\ContainerAwareCommand;
 use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
@@ -26,14 +27,33 @@ class PeopleRemoveRedundantCommand extends ContainerAwareCommand
         $em = $this->getContainer()->get('doctrine.orm.entity_manager');
         $people_res = $em->getRepository('ActsCamdramBundle:Person');
         $query = $people_res->createQueryBuilder('p')
-            ->leftJoin('ActsCamdramBundle:Role', 'r', Expr\Join::WITH, 'p.id = r.person_id')
-            ->where('r.id is null')
+            ->leftJoin('p.roles', 'r')
+            ->where('p.mapped_to is null')
+            ->andWhere('r.id is null')
             ->getQuery();
+
         $people = $query->getResult();
-        foreach ($people as $p) {
-            $em->remove($p);
-            echo 'Deleted '.$p->getName()."\r\n";
+        foreach ($people as $person) {
+            $this->deletePerson($person, $output);
         }
         $em->flush();
+    }
+
+    private function deletePerson(Person $person, OutputInterface $output)
+    {
+        $em = $this->getContainer()->get('doctrine.orm.entity_manager');
+        $people_res = $em->getRepository('ActsCamdramBundle:Person');
+
+        $mapped_people = $people_res->findBy(array('mapped_to' => $person));
+        foreach ($mapped_people as $mapped_person) {
+            $this->deletePerson($mapped_person, $output);
+        }
+        if (count($person->getUsers()) == 0 && count($person->getExternalUsers()) == 0) {
+            $em->remove($person);
+            $output->writeln('Deleted '.$person->getName());
+        }
+        else {
+            $person->setMappedTo(null);
+        }
     }
 }
