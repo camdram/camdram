0632cba8b40971b2156c98d14494db6c9872d561
Peter Hoyes <pahoyes@gmail.com>
2015-05-17 16:43:32 +0100

    WIP docker configuration (like Vagrant, but less overhead as it uses Linux 'containers') #87

--- cookbooks/symfony2/resources/console.rb
+++
@@ -1,7 +0,0 @@
-actions :cmd
-
-attribute :command, :kind_of => String
-attribute :path, :kind_of => String
-attribute :debug, :default => true
-attribute :env, :kind_of => String, :default => "dev"
-attribute :optional_params, :kind_of => Array, :default => []
