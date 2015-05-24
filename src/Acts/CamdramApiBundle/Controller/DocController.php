5b9d1baf7d68f3abe1d57d030df10da4f248dce8
Peter Hoyes <pahoyes@gmail.com>
2015-05-24 12:00:56 +0100

    Api doc work

---
+++ src/Acts/CamdramApiBundle/Controller/DocController.php
@@ -0,0 +1,22 @@
+<?php
+namespace Acts\CamdramApiBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Response;
+
+class DocController extends Controller
+{
+
+    public function indexAction()
+    {
+        return $this->render('ActsCamdramApiBundle:Doc:index.html.twig');
+    }
+
+    public function docAction()
+    {
+        $data = $this->get('nelmio_api_doc.extractor.api_doc_extractor')->all();
+        $formatter = $this->get('acts.camdram_api.service.api_doc_formatter');
+        return new Response($formatter->format($data));
+    }
+
+}
