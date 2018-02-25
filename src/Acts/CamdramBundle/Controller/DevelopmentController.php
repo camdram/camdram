c569d672344de33dbbd06f8127fc08ef552256c2
Peter Hoyes <pahoyes@gmail.com>
2018-02-25 15:44:30 +0000

    Catch Github API exceptions and display error message

--- src/Acts/CamdramBundle/Controller/DevelopmentController.php
+++ src/Acts/CamdramBundle/Controller/DevelopmentController.php
@@ -25,27 +25,34 @@ class DevelopmentController extends Controller
          * 
          * @var \Github\Client $api
          */
-        $github = $this->get('github.api');
-        $github->authenticate($this->getParameter('github_id'),
-            $this->getParameter('github_secret'), \Github\Client::AUTH_URL_CLIENT_ID);
-        $owner = 'camdram';
-        $repoName = 'camdram';
-
-        $repo = $github->api('repo')->show($owner, $repoName);
-        $inprogress = $github->api('issues')->all($owner, $repoName, ['labels' => 'in-progress']);
-        $recent = $github->api('issues')->all($owner, $repoName, ['state' => 'open', 'sort' => 'created']);
-        $fixed = $github->api('issues')->all($owner, $repoName, ['state' => 'closed', 'sort' => 'updated']);
-        
-        $data = array(
-            'repo' => $repo,
-            'inprogress' => array_slice($inprogress, 0, 10),
-            'recent' => array_slice($recent, 0, 10),
-            'fixed' => array_slice($fixed, 0, 10),
-        );
-
-        $response = $this->render('ActsCamdramBundle:Development:activity.html.twig', $data);
-        $response->setSharedMaxAge(60 * 15);
-
-        return $response;
+        try
+        {
+            $github = $this->get('github.api');
+            $github->authenticate($this->getParameter('github_id'),
+                $this->getParameter('github_secret'), \Github\Client::AUTH_URL_CLIENT_ID);
+            $owner = 'camdram';
+            $repoName = 'camdram';
+    
+            $repo = $github->api('repo')->show($owner, $repoName);
+            $inprogress = $github->api('issues')->all($owner, $repoName, ['labels' => 'in-progress']);
+            $recent = $github->api('issues')->all($owner, $repoName, ['state' => 'open', 'sort' => 'created']);
+            $fixed = $github->api('issues')->all($owner, $repoName, ['state' => 'closed', 'sort' => 'updated']);
+            
+            $data = array(
+                'repo' => $repo,
+                'inprogress' => array_slice($inprogress, 0, 10),
+                'recent' => array_slice($recent, 0, 10),
+                'fixed' => array_slice($fixed, 0, 10),
+            );
+    
+            $response = $this->render('ActsCamdramBundle:Development:activity.html.twig', $data);
+            $response->setSharedMaxAge(60 * 15);
+    
+            return $response;
+        }
+        catch (\Github\Exception\RuntimeException $ex)
+        {
+            return $this->render('ActsCamdramBundle:Development:github-error.html.twig');
+        }
     }
 }
