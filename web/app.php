0181333b7c48aad3f8ed7b48c4750a0110220372
Peter Hoyes <pahoyes@gmail.com>
2018-07-23 20:52:11 +0100

    Actually enable Symfony HTTP cache

--- web/app.php
+++ web/app.php
@@ -20,10 +20,10 @@ $apcLoader->register(true);
 
 $kernel = new AppKernel('prod', false);
 $kernel->loadClassCache();
-//$kernel = new AppCache($kernel);
+$kernel = new AppCache($kernel);
 
 // When using the HttpCache, you need to call the method in your front controller instead of relying on the configuration parameter
-//Request::enableHttpMethodParameterOverride();
+Request::enableHttpMethodParameterOverride();
 $request = Request::createFromGlobals();
 $response = $kernel->handle($request);
 $response->send();
