a99a64add7f82a6ccaf5e07594016e628d25fc54
Peter Hoyes <pahoyes@gmail.com>
2018-08-05 13:06:45 +0100

    Use subscriber instead of listener for security events Listener config in services.yml seeems to be overridden by auto-generated service if service name == class name so no emails being sent

--- src/Acts/CamdramSecurityBundle/EventListener/LastLoginTimeListener.php
+++ src/Acts/CamdramSecurityBundle/EventListener/LastLoginTimeListener.php
@@ -4,8 +4,10 @@ namespace Acts\CamdramSecurityBundle\EventListener;
 
 use Acts\CamdramSecurityBundle\Entity\User;
 use Symfony\Component\Security\Http\Event\InteractiveLoginEvent;
-use Doctrine\ORM\EntityManager;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\Security\Core\Authentication\Token\UsernamePasswordToken;
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+use Symfony\Component\Security\Http\SecurityEvents;
 use HWI\Bundle\OAuthBundle\Security\Core\Authentication\Token\OAuthToken;
 
 /**
@@ -13,13 +15,20 @@ use HWI\Bundle\OAuthBundle\Security\Core\Authentication\Token\OAuthToken;
  *
  * Updates the 'last_login_at' field of the user every time he/she logs in
  */
-class LastLoginTimeListener
+class LastLoginTimeListener implements EventSubscriberInterface
 {
     private $entityManager;
 
-    public function __construct(EntityManager $entityManager)
+    public function __construct(EntityManagerInterface $entityManager)
     {
         $this->entityManager = $entityManager;
+    }
+
+    public static function getSubscribedEvents()
+    {
+        return [
+            SecurityEvents::INTERACTIVE_LOGIN => 'onAuthenticationSuccess',
+        ];
     }
 
     /**
