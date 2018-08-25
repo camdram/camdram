a03a37c77359a099001ec3b67ed3910f14d55ac9
Peter Hoyes <pahoyes@gmail.com>
2018-08-25 14:04:56 +0100

    Fix AdminVoter after ClassIdentity removal #492

--- tests/CamdramSecurityBundle/Security/Acl/Voter/AdminVoterTest.php
+++ tests/CamdramSecurityBundle/Security/Acl/Voter/AdminVoterTest.php
@@ -3,6 +3,8 @@
 namespace Camdram\Tests\CamdramSecurityBundle\Security\Acl\Voter;
 
 use Acts\CamdramBundle\Entity\Show;
+use Acts\CamdramBundle\Entity\Society;
+use Acts\CamdramBundle\Entity\Venue;
 use Acts\CamdramSecurityBundle\Security\Acl\Voter\AdminVoter;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Security\Core\Authentication\Token\UsernamePasswordToken;
@@ -20,25 +22,41 @@ class AdminVoterTest extends TestCase
         $this->voter = new AdminVoter();
     }
 
-    public function testAdmin()
+    public function testAdminShow()
     {
-        $token = new UsernamePasswordToken('testuser', 'password', 'public', array('ROLE_ADMIN'));
-        $show = new Show();
-        $attributes = array('EDIT');
-        $this->assertEquals(AdminVoter::ACCESS_GRANTED, $this->voter->vote($token, $show, $attributes));
+        $token = new UsernamePasswordToken('testuser', 'password', 'public', ['ROLE_ADMIN']);
+        $this->assertEquals(AdminVoter::ACCESS_GRANTED, $this->voter->vote($token, Show::class, ['CREATE']));
+        $this->assertEquals(AdminVoter::ACCESS_GRANTED, $this->voter->vote($token, new Show, ['EDIT']));
+        $this->assertEquals(AdminVoter::ACCESS_GRANTED, $this->voter->vote($token, new Show, ['DELETE']));
+    }
+
+    public function testAdminSociety()
+    {
+        $token = new UsernamePasswordToken('testuser', 'password', 'public', ['ROLE_ADMIN']);
+        $this->assertEquals(AdminVoter::ACCESS_GRANTED, $this->voter->vote($token, Society::class, ['CREATE']));
+        $this->assertEquals(AdminVoter::ACCESS_GRANTED, $this->voter->vote($token, new Society, ['EDIT']));
+        $this->assertEquals(AdminVoter::ACCESS_GRANTED, $this->voter->vote($token, new Society, ['DELETE']));
+    }
+
+    public function testAdminVenue()
+    {
+        $token = new UsernamePasswordToken('testuser', 'password', 'public', ['ROLE_ADMIN']);
+        $this->assertEquals(AdminVoter::ACCESS_GRANTED, $this->voter->vote($token, Venue::class, ['CREATE']));
+        $this->assertEquals(AdminVoter::ACCESS_GRANTED, $this->voter->vote($token, new Venue, ['EDIT']));
+        $this->assertEquals(AdminVoter::ACCESS_GRANTED, $this->voter->vote($token, new Venue, ['DELETE']));
     }
 
     public function testNonAdmin()
     {
-        $token = new UsernamePasswordToken('testuser', 'password', 'public', array());
-        $show = new Show();
-        $attributes = array('EDIT');
-        $this->assertEquals(AdminVoter::ACCESS_DENIED, $this->voter->vote($token, $show, $attributes));
+        $token = new UsernamePasswordToken('testuser', 'password', 'public', []);
+        $this->assertEquals(AdminVoter::ACCESS_DENIED, $this->voter->vote($token, Show::class, ['CREATE']));
+        $this->assertEquals(AdminVoter::ACCESS_DENIED, $this->voter->vote($token, new Show, ['EDIT']));
+        $this->assertEquals(AdminVoter::ACCESS_DENIED, $this->voter->vote($token, new Show, ['DELETE']));
     }
 
     public function testNonCamdramObject()
     {
-        $token = new UsernamePasswordToken('testuser', 'password', 'public', array('ROLE_ADMIN'));
+        $token = new UsernamePasswordToken('testuser', 'password', 'public', ['ROLE_ADMIN']);
         $request = new Request();
         $attributes = array('EDIT');
         $this->assertEquals(AdminVoter::ACCESS_ABSTAIN, $this->voter->vote($token, $request, $attributes));
