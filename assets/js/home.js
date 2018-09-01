074e7bb7c5cf159f1b54f57024d231191e1af485
George Bateman <george.bateman16@gmail.com>
2018-09-01 20:46:17 +0100

    Replace scrolling diary nav
    New navigation takes slightly more space but is much faster to use, especially on narrow screens. Condensed by default on mobile (<600px).

--- assets/js/home.js
+++ assets/js/home.js
@@ -71,91 +71,20 @@ $(function() {
             })
         }
 
-        var get_limits = function() {
-            var max_left = $nav.offset().left + 30;
-            var min_left = max_left - 50 + $nav.width() - 60;
-            $('li', $nav).each(function() {
-                min_left -= $(this).outerWidth();
-            })
-            return [min_left, max_left];
-        }
-        var focus_current = function(time) {
-            var left = - $('li.current', $nav).position().left + 60;
-
-            var limits = get_limits();
-            var limits_offset = $('#home-diary-nav').offset().left;
-            if (left < limits[0] - limits_offset) left = limits[0] - limits_offset;
-            if (left > limits[1] - limits_offset) left = limits[1] - limits_offset;
-
-            $('ul', $nav).animate({'left': left}, time);
-        }
-        focus_current(0);
-        var clickenable = true;
         $('li.week-link', $nav).click(function() {
-            if (clickenable) {
-                if ($(this).hasClass('current')) return;
-                var date = $(this).attr('data-week-start');
+            if ($(this).hasClass('current')) return;
+            var date = $(this).attr('data-week-start');
 
-                var last_sel = $('li.current', $nav);
-                $('li', $nav).removeClass('current');
-                $(this).addClass('current');
-                var direction =  (last_sel.prevAll('.current').length > 0) ? 'ltr' : 'rtl';
+            var last_sel = $('li.current', $nav);
+            $('li', $nav).removeClass('current');
+            $(this).addClass('current');
+            var direction =  (last_sel.prevAll('.current').length > 0) ? 'ltr' : 'rtl';
 
-                load_new_diary(Routing.generate('acts_camdram_diary_single_week', {date: date}), direction);
-
-                var left = $(this).offset().left - $nav.offset().left;
-                var max_right = $nav.width() - $(this).width() - 60;
-                if ($(document).width() < 500) {
-                    if (left < 40 || left > max_right) {
-                        focus_current(200);
-                    }
-                }
-                else {
-                    if (left < 40) move(200);
-                    if (left > max_right) move(-200);
-                }
-            }
+            load_new_diary(Routing.generate('acts_camdram_diary_single_week', {date: date}), direction);
         });
 
-        //Slider functions
-
-        var $slider = $('ul', $nav);
-
-        var limits = get_limits();
-        $slider.draggable({
-            axis: 'x',
-            containment: [limits[0], 0, limits[1], 0],
-            distance: 2,
-            start: function() { clickenable = false;},
-            stop: function() { window.setTimeout(function() { clickenable = true; }, 100); }
-        })
-        $(window).resize(function() {
-            var limits = get_limits();
-            $slider.draggable( "option", "containment", [limits[0], 0, limits[1], 0]);
-        });
-
-        var move = function(offset) {
-            var left = parseInt($slider.css('left'));
-            left += offset;
-            var limits = get_limits();
-            var limits_offset = $('#home-diary-nav').offset().left;
-            if (left < limits[0] - limits_offset) left = limits[0] - limits_offset;
-            if (left > limits[1] - limits_offset) left = limits[1] - limits_offset;
-
-            $slider.animate({left: left}, 200);
-        }
-
-        var get_move_size = function() {
-            return $('#wrapper > section').width() / 3;
-        }
-
-        $('.left-link a', $container).click(function(e) {
-            move(get_move_size());
-            e.preventDefault();
-        });
-        $('.right-link a', $container).click(function(e) {
-            move(-get_move_size());
-            e.preventDefault();
+        $('.diary-expand', $nav).click(function() {
+            document.querySelector('#home-diary-nav').classList.add('expanded');
         });
     }
 })
