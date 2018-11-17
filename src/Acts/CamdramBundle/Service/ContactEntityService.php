ca909e372905896187e6310c7eac35e4b2ac4293
Charlie Jonas <devel@charliejonas.co.uk>
2018-11-17 15:45:49 +0000

    Improve entity contact emails
    Sends a line in the email body which tells the user why they are being contacted.
    
    Fixes #430

--- src/Acts/CamdramBundle/Service/ContactEntityService.php
+++ src/Acts/CamdramBundle/Service/ContactEntityService.php
@@ -20,11 +20,12 @@ class ContactEntityService
     public function emailEntity($entity, $from_name, $from_email, $subject, $message)
     {
         $recipients = $this->findRecipients($entity);
+        $msg = 'You are receiving this email because you manage '.$entity->getName().' on Camdram.\n\n'.$message;
 
         $message = (new \Swift_Message('[Camdram] ' . $subject))
             ->setFrom($from_email, $from_name)
             ->setTo($recipients)
-            ->setBody($message)
+            ->setBody($msg)
             ;
 
         $this->mailer->send($message);
