f5676aa8aab62d7bde18013e12f5588d4dbffa3c
Peter Hoyes <pahoyes@gmail.com>
2018-11-24 17:27:20 +0000

    Testing tweaks, mainly to make it easier to run phpunit tests in isolation  * Define test DB url in config_test.yml  * Use custom boostrap.php to create test DB if it doesn't exist

---
+++ tests/bootstrap.php
@@ -0,0 +1,24 @@
+<?php
+require(__DIR__.'/../vendor/autoload.php');
+
+use Symfony\Component\Process\PhpExecutableFinder;
+use Symfony\Component\Process\Process;
+
+//Create schema if the SQLite DB doesn't already exist
+$testDbPath = __DIR__.'/../app/cache/test/test.db';
+if (!file_exists($testDbPath) || filesize($testDbPath) == 0) {
+
+    echo "Generating test DB schema...\n";
+
+    //Find the PHP executable
+    $phpFinder = new PhpExecutableFinder();
+    if (!$phpPath = $phpFinder->find()) {
+        throw new \RuntimeException('The php executable could not be found');
+    }
+
+    $process = new Process($phpPath.' app/console doctrine:schema:create');
+    $process->run();
+    if (!$process->isSuccessful()) {
+        throw new \RuntimeException('An error occurred generate the test DB schema');
+    }
+}
