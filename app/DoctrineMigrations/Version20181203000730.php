d9f4ca72c73dc64897f3ca94fff6810e4d4e8a03
George Bateman <george.bateman16@gmail.com>
2019-01-10 09:45:47 +0000

    Bump doctrine/doctrine-migrations-bundle from 1.3.2 to 2.0.0
    Bumps [doctrine/doctrine-migrations-bundle](https://github.com/doctrine/DoctrineMigrationsBundle) from 1.3.2 to 2.0.0.
    - [Release notes](https://github.com/doctrine/DoctrineMigrationsBundle/releases)
    - [Changelog](https://github.com/doctrine/DoctrineMigrationsBundle/blob/master/Changelog.md)
    - [Commits](https://github.com/doctrine/DoctrineMigrationsBundle/compare/v1.3.2...v2.0.0)
    
    Signed-off-by: dependabot[bot] <support@dependabot.com>
    
    Necessary changes made to migration files.

--- app/DoctrineMigrations/Version20181203000730.php
+++ app/DoctrineMigrations/Version20181203000730.php
@@ -2,7 +2,7 @@
 
 namespace Application\Migrations;
 
-use Doctrine\DBAL\Migrations\AbstractMigration;
+use Doctrine\Migrations\AbstractMigration;
 use Doctrine\DBAL\Schema\Schema;
 
 /**
@@ -13,7 +13,7 @@ class Version20181203000730 extends AbstractMigration
     /**
      * @param Schema $schema
      */
-    public function up(Schema $schema)
+    public function up(Schema $schema): void
     {
         // New table and column
         $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
@@ -39,7 +39,7 @@ class Version20181203000730 extends AbstractMigration
     /**
      * @param Schema $schema
      */
-    public function down(Schema $schema)
+    public function down(Schema $schema): void
     {
         // this down() migration is auto-generated, please modify it to your needs
         $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
--- app/DoctrineMigrations/Version20181212175500.php
+++ app/DoctrineMigrations/Version20181212175500.php
@@ -1,6 +1,6 @@
 <?php
 namespace Application\Migrations;
-use Doctrine\DBAL\Migrations\AbstractMigration;
+use Doctrine\Migrations\AbstractMigration;
 use Doctrine\DBAL\Schema\Schema;
 /**
  * Auto-generated Migration: Please modify to your needs!
@@ -10,7 +10,7 @@ class Version20181212175500 extends AbstractMigration
     /**
      * @param Schema $schema
      */
-    public function up(Schema $schema)
+    public function up(Schema $schema): void
     {
         // this up() migration is auto-generated, please modify it to your needs
         $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
@@ -19,7 +19,7 @@ class Version20181212175500 extends AbstractMigration
     /**
      * @param Schema $schema
      */
-    public function down(Schema $schema)
+    public function down(Schema $schema): void
     {
         // this down() migration is auto-generated, please modify it to your needs
         $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
