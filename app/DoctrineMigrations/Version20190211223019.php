ad97661911e8afd8ab996cb3537fad98d2b08e24
Charlie Jonas <charlie@charliejonas.co.uk>
2019-02-11 22:37:14 +0000

    Make database migration the most recent

--- app/DoctrineMigrations/Version20190211223019.php
+++ app/DoctrineMigrations/Version20190211223019.php
@@ -8,7 +8,7 @@ use Doctrine\DBAL\Schema\Schema;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-class Version20181210165547 extends AbstractMigration
+class Version20190211223019 extends AbstractMigration
 {
     /**
      * @param Schema $schema
