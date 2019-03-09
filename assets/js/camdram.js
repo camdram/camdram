camdram/camdram

commit 5caacd415aead8392a862068f9460ecf45a3bec8

Introduce content security policy. Trial run of #585

app/Resources/views/form/fields.html.twig
@@ -2,16 +2,17 @@
     {% spaceless %}
         {% set text = attribute(form.children, text_id) %}
         {% set hidden = attribute(form.children, hidden_id) %}
-        {{ form_widget(text) }}
+        {{ form_widget(text, {'attr': {
+           'data-entitysearch-prefetch': prefetch,
+           'data-entitysearch-route': route } }) }}
         {{ form_widget(hidden) }}
 
         <script>
+            {#- begin-CSP-permitted-script -#}
             $(function(){
-                $('#{{ text.vars.id }}').entitySearch({
-                    route: '{{ route }}',
-                    prefetch: {% if prefetch %}true{% else %}false{% endif %}
-                });
+                $(document).entitySearch({'auto': 1});
             })
+            {#- end-CSP-permitted-script -#}
         </script>
 
     {% endspaceless %}

assets/js/camdram.js
@@ -2,6 +2,9 @@ import Dropzone from 'dropzone';
 import Routing from 'router';
 import Bloodhound from 'typeahead.js'
 
+// Leak Routing to the global scope so that inline scripts work.
+window.Routing = Routing;
+
 ;(function($, window) {
     String.prototype.truncate = function(length) {
         str = jQuery.trim(this).substring(0, length)
@@ -168,10 +171,25 @@ import Bloodhound from 'typeahead.js'
     }
 
     $.fn.entitySearch = function(options) {
-       var options = $.extend({
-           placeholder: 'start typing to search',
-           prefetch : true
-       }, options);
+        if (options['auto']) {
+           this.find('[data-entitysearch-route]').each(function() {
+               $(this).entitySearch({
+                   placeholder: 'start typing to search',
+                   prefetch: this.getAttribute('data-entitysearch-prefetch'),
+                   route: this.getAttribute('data-entitysearch-route')
+               });
+               // Ensure that entitySearch is fired only once per element,
+               // regardless of how many times entitySearch({auto}) is called.
+               this.removeAttribute('data-entitysearch-prefetch');
+               this.removeAttribute('data-entitysearch-route');
+           });
+           return;
+        }
+
+        var options = $.extend({
+            placeholder: 'start typing to search',
+            prefetch : true
+        }, options);
         var $self = $(this);
 
         var tokenize = function(str) {

app/Resources/views/admin/log/index.html.twig
@@ -62,6 +62,7 @@
         </div>
 </div>
 <script>
+    {#- begin-CSP-permitted-script -#}
     $('#logTabs a.title').click(function(e) {
         var $self = $(this);
         $.get($self.attr('href'), function(response) {
@@ -71,5 +72,6 @@
         })
     });
     $('#logTabs a.title.active').click();
+    {#- end-CSP-permitted-script -#}
 </script>
 {% endblock %}

app/Resources/views/admin/user/index.html.twig
@@ -52,6 +52,7 @@
 
 {% block javascripts %}
 <script>
+    {#- begin-CSP-permitted-script -#}
     $(function() {
         $('#user_search_box').typeahead({
             name: 'users_search',
@@ -61,5 +62,6 @@
             document.location = Routing.generate('get_user', {identifier: datum.id})
         });
     })
+    {#- end-CSP-permitted-script -#}
 </script>
 {% endblock %}

app/Resources/views/api/default/test_console.html.twig
@@ -53,6 +53,7 @@
     </div>
 
     <script>
+    {#- begin-CSP-permitted-script -#}
         $('#test_console_form').submit(function(e) {
             e.preventDefault();
             var method = $('#method').val();
@@ -88,6 +89,7 @@
             $('#url').val($(this).attr('href'));
             $('#test_console_form').submit();
         })
+    {#- end-CSP-permitted-script -#}
     </script>
 
 {% endblock %}

app/Resources/views/base.html.twig
@@ -4,13 +4,15 @@
         <!-- Global site tag (gtag.js) - Google Analytics -->
 	<script async src="https://www.googletagmanager.com/gtag/js?id=UA-37770738-2"></script>
 	<script>
+                {#- begin-CSP-permitted-script -#}
 		window.dataLayer = window.dataLayer || [];
 		function gtag(){dataLayer.push(arguments);}
 		gtag('js', new Date());
-		
+
 		gtag('config', 'UA-37770738-2');
-	</script>
-	
+	        {#- end-CSP-permitted-script -#}
+        </script>
+
 	<meta charset="utf-8" />
 
         {# Block pages from appearing in search engines if dev site #}
@@ -34,13 +36,13 @@
 
         <title>{% block title %}{% endblock %} - Camdram</title>
 
-        <link rel="stylesheet" type="text/css" href="//cdnjs.cloudflare.com/ajax/libs/cookieconsent2/3.0.3/cookieconsent.min.css" />
-        <link href="//netdna.bootstrapcdn.com/font-awesome/4.1.0/css/font-awesome.css" rel="stylesheet"/>
+        <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/cookieconsent2/3.0.3/cookieconsent.min.css" />
+        <link href="https://netdna.bootstrapcdn.com/font-awesome/4.1.0/css/font-awesome.css" rel="stylesheet"/>
         <link href="{{ asset('build/app.css') }}" rel="stylesheet" />     
         {% block stylesheets %}{% endblock %}
 
-        <script src="//ajax.googleapis.com/ajax/libs/jquery/1.8.3/jquery.min.js"></script>
-        <script src="//ajax.googleapis.com/ajax/libs/jqueryui/1.12.1/jquery-ui.min.js"></script>
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.8.3/jquery.min.js"></script>
+        <script src="https://ajax.googleapis.com/ajax/libs/jqueryui/1.12.1/jquery-ui.min.js"></script>
         <script src="{{ asset('build/manifest.js') }}"></script>
         <script src="{{ asset('build/vendor.js') }}"></script>
         <script src="{{ asset('build/app.js') }}"></script>

app/Resources/views/pending_access/edit.html.twig
@@ -56,13 +56,12 @@
 {% endif %}
 {% for admin in admins %}
     <div id="admin_{{ admin.id }}">
-        <form action="{{ path(revoke_admin_route, {identifier: entity.slug, uid: admin.id}) }}" method="post" id="revoke_admin_form_{{ admin.id }}">
+        <form action="{{ path(revoke_admin_route, {identifier: entity.slug, uid: admin.id}) }}" method="post" id="revoke_admin_form_{{ admin.id }}" class="inline-block">
             <input type="hidden" name="_method" value="DELETE" />
             <input type="hidden" name="_token" value="{{ csrf_token('delete_admin') }}" />
+            <button title="Revoke this admin." class="fa fa-fw fa-trash-o text-button"></button>
         </form>
-    <span title="Revoke this admin.">
-        <a href="javascript:document.getElementById('revoke_admin_form_{{ admin.id }}').submit()" class="fa fa-fw fa-trash-o"></a></span>&ensp;
-    {%- if admin.person is defined -%}
+    {% if admin.person is defined -%}
         {%- if admin.person -%}
             <a href="{{ path('get_person', {identifier: admin.person.slug}) }}">{{ admin.name }}</a>
         {%- else -%}
@@ -78,13 +77,12 @@
 {% endif %}
 {% for pending in pending_admins %}
     <div id="pending_{{ pending.id }}">
-    <form action="{{ path(revoke_pending_admin_route, {identifier: entity.slug, uid: pending.id}) }}" method="post" id="revoke_pending_admin_form_{{ pending.id }}">
+    <form action="{{ path(revoke_pending_admin_route, {identifier: entity.slug, uid: pending.id}) }}" method="post" id="revoke_pending_admin_form_{{ pending.id }}" class="inline-block">
         <input type="hidden" name="_method" value="DELETE" />
         <input type="hidden" name="_token" value="{{ csrf_token('delete_pending_admin') }}" />
+        <button title="Revoke this pending admin." class="fa fa-fw fa-trash-o text-button"></button>
     </form>
 
-    <span title="Revoke this pending admin.">
-        <a href="javascript:document.getElementById('revoke_pending_admin_form_{{ pending.id }}').submit()" class="fa fa-fw fa-trash-o"></a></span>&ensp;
     {{ pending.email }}
     </div>
 {% endfor %}

app/Resources/views/person/merge.html.twig
@@ -39,6 +39,7 @@
 </form>
 
 <script>
+    {#- begin-CSP-permitted-script -#}
     $(function() {
         var nameFields = $('#name_fields');
         var slugFields = $('#slug_fields');
@@ -46,7 +47,7 @@
         var slugRadio = $('#form_search_by_1');
 
         var createLink = function(slug) {
-            var url = "{{ path('get_person', {identifier: 'THESLUG'}) }}".replace("THESLUG", slug);
+            var url = Routing.generate('get_person', {identifier: slug}, true);
             return $('<a/>').attr('href', url).attr('target', '_blank').text(url);
         }
 
@@ -82,6 +83,7 @@
         nameRadio.change(updateMergeForm);
         slugRadio.change(updateMergeForm);
     })
+    {#- end-CSP-permitted-script -#}
 </script>
 
 {% endblock %}

commit 11ab41b540d195f715da845d2bd722b8a7025cb1

Fix bug introduced into entitySearch by 5caacd415

app/Resources/views/form/fields.html.twig
@@ -3,18 +3,9 @@
         {% set text = attribute(form.children, text_id) %}
         {% set hidden = attribute(form.children, hidden_id) %}
         {{ form_widget(text, {'attr': {
-           'data-entitysearch-prefetch': prefetch,
+           'data-entitysearch-prefetch': (prefetch ? 'true' : 'false'),
            'data-entitysearch-route': route } }) }}
         {{ form_widget(hidden) }}
-
-        <script>
-            {#- begin-CSP-permitted-script -#}
-            $(function(){
-                $(document).entitySearch({'auto': 1});
-            })
-            {#- end-CSP-permitted-script -#}
-        </script>
-
     {% endspaceless %}
 {% endblock %}
 

assets/js/camdram.js
@@ -69,6 +69,7 @@ window.Routing = Routing;
     var fixHtml = function(elementsToFix){
         $('.news_media', elementsToFix).newsFeedMedia();
         $('.button-destructive').deleteLink();
+        $(elementsToFix).entitySearch({auto: 1});
         createTabContainers();
 
         if (!supportsDateInput()) {
@@ -173,16 +174,17 @@ window.Routing = Routing;
     $.fn.entitySearch = function(options) {
         if (options['auto']) {
            this.find('[data-entitysearch-route]').each(function() {
+               console.log(this);
                $(this).entitySearch({
                    placeholder: 'start typing to search',
-                   prefetch: this.getAttribute('data-entitysearch-prefetch'),
+                   prefetch: this.getAttribute('data-entitysearch-prefetch') == 'true',
                    route: this.getAttribute('data-entitysearch-route')
                });
-               // Ensure that entitySearch is fired only once per element,
-               // regardless of how many times entitySearch({auto}) is called.
-               this.removeAttribute('data-entitysearch-prefetch');
-               this.removeAttribute('data-entitysearch-route');
            });
+           // Ensure that entitySearch is fired only once per element,
+           // regardless of how many times entitySearch({auto}) is called.
+           this.find('[data-entitysearch-prefetch]').removeAttr('data-entitysearch-prefetch');
+           this.find('[data-entitysearch-route]').removeAttr('data-entitysearch-route');
            return;
         }
 