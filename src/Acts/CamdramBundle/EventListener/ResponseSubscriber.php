944ea1b8d2ebf240f4ff21d1ec91cd2235162532
George Bateman <george.bateman16@gmail.com>
2019-03-10 10:34:26 +0000

    Improvements to CSP

--- src/Acts/CamdramBundle/EventListener/ResponseSubscriber.php
+++ src/Acts/CamdramBundle/EventListener/ResponseSubscriber.php
@@ -50,9 +50,12 @@ class ResponseSubscriber implements EventSubscriberInterface
             trigger_error('csp_hashes.txt does not exist, run ./gen_csp_hashes.py!');
         } else {
             $this->policy = "block-all-mixed-content; " .
-            "default-src 'self' https://www.google-analytics.com https://*.openstreetmap.org; " .
+            "default-src 'self' https://www.google-analytics.com https://*.openstreetmap.org " .
+            "*.googleusercontent.com *.facebook.com *.googletagmanager.com; " .
             "font-src 'self' https://netdna.bootstrapcdn.com; " .
-            "script-src 'self' https://www.googletagmanager.com https://ajax.googleapis.com https://www.google-analytics.com ".
+            "child-src *.google.com; " . # We don't put frames around our own content
+            "frame-src *.google.com; " . # We don't put frames around our own content
+            "script-src 'self' https://www.googletagmanager.com https://ajax.googleapis.com https://www.google-analytics.com *.google.com *.gstatic.com ".
             $script_hashes .
             "; style-src 'self' 'unsafe-inline' https://cdnjs.cloudflare.com https://netdna.bootstrapcdn.com";
             if ($onProd) {
