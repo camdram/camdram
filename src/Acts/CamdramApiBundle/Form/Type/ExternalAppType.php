89819c7d84e8d605cd3ddb2325c1d3792b04fab0
Peter Hoyes <pahoyes@gmail.com>
2019-10-19 21:50:03 +0100

    Add captcha to API app form #724

--- src/Acts/CamdramApiBundle/Form/Type/ExternalAppType.php
+++ src/Acts/CamdramApiBundle/Form/Type/ExternalAppType.php
@@ -2,11 +2,15 @@
 
 namespace Acts\CamdramApiBundle\Form\Type;
 
+use EWZ\Bundle\RecaptchaBundle\Form\Type\EWZRecaptchaType;
+use EWZ\Bundle\RecaptchaBundle\Validator\Constraints\IsTrue as RecaptchaTrue;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\Form\Extension\Core\Type\UrlType;
+use Symfony\Component\Form\FormEvent;
+use Symfony\Component\Form\FormEvents;
 
 use Acts\CamdramApiBundle\Entity\ExternalApp;
 
@@ -24,6 +28,24 @@ class ExternalAppType extends AbstractType
             ->add('app_type', AppType::class)
             ->add('website', UrlType::class)
             ->add('redirect_uris_string', TextareaType::class, array('label' => 'Redirect URIs', 'required' => false))
+            ->addEventListener(FormEvents::PRE_SET_DATA, function (FormEvent $event) {
+                $id = $event->getData() ? $event->getData()->getId() : null;
+                $form = $event->getForm();
+                //Only add captcha when creating a new ExternalApp
+                if (!$id) {
+                    $form->add('captcha', EWZRecaptchaType::class, [
+                        'attr' => [
+                            'options' => [
+                                'theme' => 'clean'
+                            ]
+                        ],
+                        'mapped' => false,
+                        'constraints' => [
+                            new RecaptchaTrue()
+                        ]
+                    ]);
+                }
+            });
         ;
     }
 
