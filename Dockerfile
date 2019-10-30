cc126797fe4338a3b238f80e3c10adc00aca0a0b
Charlie Jonas <charlie@charliejonas.co.uk>
2019-10-30 19:59:07 +0000

    Use MariaDB with Docker Compose

--- Dockerfile
+++ Dockerfile
@@ -4,6 +4,7 @@ WORKDIR /usr/src/camdram
 
 RUN apt-get update && \
     apt-get install -y git zip unzip curl wget nano vim python3 && \
+    docker-php-ext-install -j$(nproc) pdo pdo_mysql && \
     wget https://raw.githubusercontent.com/composer/getcomposer.org/76a7060ccb93902cd7576b67264ad91c8a2700e2/web/installer -O - -q | php -- --quiet && \
     mv composer.phar /usr/local/bin/composer && \
     apt-get -q clean && \
--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,10 +1,15 @@
 #!/bin/sh
 set -e
 
+# Sleep while we wait for MariaDB to startup
+sleep 7
+
 if [ ! -d /usr/src/camdram/vendor ]; then
+  sleep 10
   /usr/local/bin/composer install
-  app/console camdram:database:refresh
-  app/console camdram:assets:download
+  php app/console doctrine:database:create
+  php app/console doctrine:migrations:migrate --no-interaction
+  php app/console doctrine:fixtures:load --no-interaction
   php app/console camdram:assets:download
   python3 ./gen-csp-hashes.py
 fi
