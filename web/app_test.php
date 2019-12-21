560e9bf84849c6453016c501b68f951d07cdbb7c
George Bateman <george.bateman16@gmail.com>
2019-12-21 12:25:03 +0000

    Remove deprecated loadClassCache()

--- web/app_test.php
+++ web/app_test.php
@@ -18,7 +18,6 @@ $loader = require_once __DIR__.'/../app/bootstrap.php.cache';
 require_once __DIR__.'/../app/AppKernel.php';
 
 $kernel = new AppKernel('test', true);
-$kernel->loadClassCache();
 Request::enableHttpMethodParameterOverride();
 $request = Request::createFromGlobals();
 $response = $kernel->handle($request);
