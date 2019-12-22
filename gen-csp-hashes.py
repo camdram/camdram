8d6f9f5eb81364c6b57df49179b392aa606b9061
George Bateman <george.bateman16@gmail.com>
2019-12-22 22:24:32 +0000

    Switch to a custom jQuery UI with touch events hack
    jQuery UI from the official download builder and toucb events code from http://touchpunch.furf.com, MIT/GPL2

--- gen-csp-hashes.py
+++ gen-csp-hashes.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 
+import argparse
 import base64
 import hashlib
 import os
@@ -12,18 +13,28 @@ import sys
 regex = re.compile('(?<={#- begin-CSP-permitted-script -#}).*?(?={#- end-CSP-permitted-script -#})', flags=re.DOTALL | re.MULTILINE)
 sha256 = hashlib.sha256()
 
-debug_mode = (len(sys.argv) > 1) and ('--debug-mode' in sys.argv)
+parser = argparse.ArgumentParser(description='Generate part of CSP header from app/Resources/views.')
+parser.add_argument('--debug-mode', action='store_true', help='Print lots of info (use a pager!)')
+parser.add_argument('--set-unsafe', action='store_true', help='Allow any inline JS, don\'t generate hashes.')
+args = parser.parse_args()
 
 with open('web/build/csp_hashes.txt', 'w', encoding='utf-8', newline='') as outfile:
+    if args.set_unsafe:
+        print("'unsafe-inline'", end='', file=outfile)
+        sys.exit(0)
     for root, dirs, files in os.walk('app/Resources/views'):
         for name in files:
-            with open(os.path.join(root, name), encoding='utf-8', newline='') as f:
-                content = f.read()
+            try:
+                with open(os.path.join(root, name), encoding='utf-8', newline='') as f:
+                    content = f.read()
                 for match in regex.findall(content):
                     match = bytes(match.strip(), 'utf-8')
-                    if debug_mode:
+                    if args.debug_mode:
                         print(root + name, base64.b64encode(hashlib.sha256(match).digest()).decode('utf-8'))
                         print(match.decode('utf-8'))
                         print()
                     print('\'sha256-' + base64.b64encode(hashlib.sha256(match).digest()).decode('utf-8'), end='\' ', file=outfile)
+            except Exception as e:
+                print(f"WARNING: Failed to process {root}/{name}");
+                print(e);
 
