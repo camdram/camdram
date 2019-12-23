1b5c003a8ed69092138f96cd36eba49b61a80e95
George Bateman <george.bateman16@gmail.com>
2019-12-23 22:04:05 +0000

    Finish new role editing code
    Fixes #447; the search index previously wasn't updated by the bulk-add
    form.
    Closes #531.

--- src/Acts/CamdramBundle/Controller/Show/RoleController.php
+++ src/Acts/CamdramBundle/Controller/Show/RoleController.php
@@ -8,16 +8,26 @@ use Acts\CamdramBundle\Entity\Show;
 use Acts\CamdramBundle\Form\Type\RolesType;
 use Acts\CamdramBundle\Form\Type\RoleType;
 use Acts\CamdramSecurityBundle\Security\Acl\Helper;
+use Doctrine\ORM\EntityManagerInterface;
 use FOS\RestBundle\Controller\Annotations as Rest;
 use FOS\RestBundle\Controller\AbstractFOSRestController;
 use Psr\Log\LoggerInterface;
+use Symfony\Component\HttpKernel\Exception\HttpException;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 
 class RoleController extends AbstractFOSRestController
 {
+    private $em;
+
+    public function __construct(EntityManagerInterface $em)
+    {
+        $this->em = $em;
+    }
+
     protected function getEntity($identifier)
     {
-        return $this->getDoctrine()->getRepository('ActsCamdramBundle:Show')->findOneBy(array('slug' => $identifier));
+        return $this->em->getRepository('ActsCamdramBundle:Show')->findOneBy(array('slug' => $identifier));
     }
 
     /**
@@ -99,52 +109,114 @@ class RoleController extends AbstractFOSRestController
     }
 
     /**
-     * Remove a role from a show.
-     * @Rest\Delete("/shows/{identifier}/role")
+     * Add or modify a role and return relevant data for the form.
+     *
+     * Creates a new person if they're not already part of Camdram.
+     *
+     * @Rest\Patch("/shows/{identifier}/roles")
+     * @param $identifier
      */
-    public function deleteRoleAction(Request $request, Helper $helper, $autocomplete_person, $identifier)
+    public function patchRoleAction(Request $request, Helper $helper, LoggerInterface $logger, $autocomplete_person, $identifier)
     {
         $show = $this->getEntity($identifier);
         $helper->ensureGranted('EDIT', $show);
 
-        if (!$this->isCsrfTokenValid('delete_show_role', $request->request->get('_token'))) {
-            throw new BadRequestHttpException('Invalid CSRF token');
-        }
+        if ($request->request->get('id') == 'new') {
+            $role = $this->addRoleToShow($show, $request->request->get('role_type'),
+                $request->request->get('role'), $request->request->get('person'));
 
-        $em = $this->getDoctrine()->getManager();
-        $id = $request->query->get('role');
-        $role_repo = $em->getRepository('ActsCamdramBundle:Role');
-        $role = $role_repo->findOneById($id);
-        if ($role != null) {
-            $person = $role->getPerson();
-            $show->removeRole($role);
-            $role_repo->removeRoleFromOrder($role);
-            $person->removeRole($role);
-            $em->remove($role);
+            try {
+                $this->em->flush();
+                $autocomplete_person->replaceOne($role->getPerson());
+            } catch (\Elastica\Exception\ExceptionInterface $ex) {
+                $logger->warning('Failed to update search index during role edit',
+                        ['role' => $role->getId()]);
+            }
 
-            // Ensure the person is not an orphan.
-            if ($person->getRoles()->isEmpty()) {
-                $em->remove($person);
+        } else {
+            $id = $request->request->get('id');
+            $role = $this->em->getRepository('ActsCamdramBundle:Role')
+                             ->findOneById($id);
+            if (!$role) {
+                throw new HttpException(404, 'Role not found');
+            }
+            if ($role->getShow()->getId() != $show->getId()) {
+                throw new HttpException(400, 'That role is not part of that show');
+            }
+            $role->setRole($request->request->get('role'));
+
+            $oldPerson = $role->getPerson();
+            $newPerson = $this->findOrMakePerson($request->request->get('person'));
+            if ($newPerson->getId() != $oldPerson->getId()) {
+                $this->removeRoleFromPerson($role, $oldPerson);
+                $role->setPerson($newPerson);
+                $newPerson->addRole($role);
             }
 
             try {
-                $em->flush();
-
-                if ($em->contains($person)) //person isn't deleted
+                $this->em->flush();
+                $autocomplete_person->replaceOne($newPerson);
+                if ($this->em->contains($oldPerson)) //person isn't deleted
                 {
                     //Attempt to update role count in people search index
-                    $autocomplete_person->replaceOne($person);
+                    $autocomplete_person->replaceOne($oldPerson);
                 }
-            }
-            catch (\Elastica\Exception\ExceptionInterface $ex) {
-                $this->get('logger')->warning('Failed to update search index during role entry',
+            } catch (\Elastica\Exception\ExceptionInterface $ex) {
+                $logger->warning('Failed to update search index during role edit',
                         ['role' => $role->getId()]);
             }
-        } else {
-            $this->addFlash('error', 'Cannot delete role: ID not found. It may already have been deleted.');
         }
 
-        return $this->routeRedirectView('get_show', array('identifier' => $show->getSlug()));
+        return new Response(json_encode([
+            "id" => $role->getId(),
+            "person_slug" => $role->getPerson()->getSlug()
+        ]));
+    }
+
+    /**
+     * Remove a role from a show. We don't need to be told the show id.
+     * @Rest\Delete("/delete-role")
+     */
+    public function deleteRoleAction(Request $request, Helper $helper, LoggerInterface $logger, $autocomplete_person)
+    {
+        if (!$this->isCsrfTokenValid('delete_show_role', $request->request->get('_token'))) {
+            throw new HttpException(400, 'Invalid CSRF token');
+        }
+
+        $id = $request->request->get('role');
+        $role_repo = $this->em->getRepository('ActsCamdramBundle:Role');
+        $role = $role_repo->findOneById($id);
+
+        if (!$role) {
+            throw new HttpException(404, "Role not found");
+        }
+
+        $show = $role->getShow();
+        $helper->ensureGranted('EDIT', $show);
+
+        $person = $role->getPerson();
+        $show->removeRole($role);
+        $role_repo->removeRoleFromOrder($role);
+        $this->removeRoleFromPerson($role, $person);
+        $this->em->remove($role);
+
+        try {
+            $this->em->flush();
+
+            if ($this->em->contains($person)) //person isn't deleted
+            {
+                //Attempt to update role count in people search index
+                $autocomplete_person->replaceOne($person);
+            }
+        }
+        catch (\Elastica\Exception\ExceptionInterface $ex) {
+            $logger->warning('Failed to update search index during role entry',
+                    ['role' => $role->getId()]);
+        }
+
+        $response = new Response("");
+        $response->setStatusCode(Response::HTTP_NO_CONTENT);
+        return $response;
     }
 
     /**
@@ -177,13 +249,15 @@ class RoleController extends AbstractFOSRestController
      * @param $identifier
      * @Rest\Post("/shows/{identifier}/many-roles")
      */
-    public function postManyRolesAction(Request $request, Helper $helper, $identifier)
+    public function postManyRolesAction(Request $request, Helper $helper, LoggerInterface $logger, $autocomplete_person, $identifier)
     {
         $show = $this->getEntity($identifier);
         $helper->ensureGranted('EDIT', $show);
-
         $form = $this->createForm(RolesType::class, [['identifier' => $identifier]]);
         $form->handleRequest($request);
+
+        $people = [];
+
         if ($form->isValid()) {
             $data = $form->getData();
             $role_idx = ($data['ordering'] == 'role_first') ? 0 : 1;
@@ -200,14 +274,27 @@ class RoleController extends AbstractFOSRestController
                 $role = trim($lsplt[$role_idx]);
                 $name = trim($lsplt[$name_idx]);
                 if (($name != '') && ($role != '')) {
-                    /* Add a role to the show. */
-                    $this->addRoleToShow(
+                    /* Add a role to the show and record the person. */
+                    $people[] = $this->addRoleToShow(
                         $this->getEntity($identifier),
                         $data['type'],
                         $role,
                         $name
-                    );
+                    )->getPerson();
                 }
+            }
+            try {
+                $this->em->flush();
+
+                foreach ($people as $person) {
+                    if ($this->em->contains($person)) {//person isn't deleted
+                        //Attempt to update role count in people search index
+                        $autocomplete_person->replaceOne($person);
+                    }
+                }
+            }
+            catch (\Elastica\Exception\ExceptionInterface $ex) {
+                $logger->warning('Failed to update search index during bulk role entry');
             }
         } else {
             // Form not valid, hand back to user
@@ -223,37 +310,63 @@ class RoleController extends AbstractFOSRestController
      * Utility function for adding a person to this show. A new person
      * record is created if they don't already exist.
      *
+     * After this the calling function must call $em->flush, and
+     * $autocomplete_person->replaceOne($person) on each person.
+     *
      * @param Show   $show        This show.
      * @param string $role_type   The type of role ('cast', 'band', 'prod')
      * @param string $role_name   Director, Producer, Macbeth..
      * @param string $person_name The person's name
+     *
+     * Returns the new role.
      */
-    private function addRoleToShow(Show $show, $role_type, $role_name, $person_name)
+    private function addRoleToShow(Show $show, $role_type, $role_name, $person_name): Role
     {
+        if (!in_array($role_type, ['cast', 'band', 'prod'])) {
+            throw new HttpException(400, "Bad value of role_type");
+        }
         $role = new Role();
         $role->setType($role_type);
         $role->setRole($role_name);
 
-        $em = $this->getDoctrine()->getManager();
-        $person_repo = $em->getRepository('ActsCamdramBundle:Person');
-
-        /* Try and find the person. Add a new person if they don't exist. */
-        $person = $person_repo->findCanonicalPerson($person_name);
-        if ($person == null) {
-            $person = new Person();
-            $person->setName($person_name);
-            $em->persist($person);
-        }
+        $person = $this->findOrMakePerson($person_name);
         $role->setPerson($person);
         /* Append this role to the list of roles of this type. */
         $order = $this->getDoctrine()->getRepository('ActsCamdramBundle:Role')
             ->getMaxOrderByShowType($show, $role->getType());
         $role->setOrder(++$order);
         $role->setShow($show);
-        $em->persist($role);
+        $this->em->persist($role);
 
         $person->addRole($role);
         $show->addRole($role);
-        $em->flush();
+
+        return $role;
+    }
+
+    private function findOrMakePerson(string $person_name): Person
+    {
+        $person_name = trim($person_name);
+        // Normalize apostrophes.
+        $person_name = strtr($person_name, ['‘' => '\'', '’' => '\'']);
+        /* Try and find the person. Add a new person if they don't exist. */
+        $person = $this->em->getRepository('ActsCamdramBundle:Person')
+                       ->findCanonicalPerson($person_name);
+        if ($person == null) {
+            $person = new Person();
+            $person->setName($person_name);
+            $this->em->persist($person);
+        }
+        return $person;
+    }
+
+    private function removeRoleFromPerson(Role $role, Person $person)
+    {
+        $person->removeRole($role);
+
+        // Ensure the person is not an orphan.
+        if ($person->getRoles()->isEmpty()) {
+            $this->em->remove($person);
+        }
     }
 }
