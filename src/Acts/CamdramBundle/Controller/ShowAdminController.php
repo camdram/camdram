9e088178575d08438473bf0d7f7a6f78abe38d6e
Peter Hoyes <pahoyes@gmail.com>
2020-04-30 00:06:35 +0100

    Fix 500 error regression in ShowAdminController

--- src/Acts/CamdramBundle/Controller/ShowAdminController.php
+++ src/Acts/CamdramBundle/Controller/ShowAdminController.php
@@ -2,11 +2,15 @@
 
 namespace Acts\CamdramBundle\Controller;
 
+use Doctrine\ORM\EntityManagerInterface;
 use FOS\RestBundle\Controller\Annotations\RouteResource;
 use Acts\CamdramBundle\Entity\Show;
+use Acts\CamdramBundle\Service\ModerationManager;
+use Acts\CamdramSecurityBundle\Security\Acl\AclProvider;
 use FOS\RestBundle\Controller\Annotations as Rest;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
 
 /**
  * Class ShowAdminController
@@ -14,18 +18,17 @@ use Symfony\Component\HttpFoundation\Request;
  * Controller for site-wide show administration.
  *
  * @RouteResource("My-Show")
+ * @IsGranted("ROLE_USER")
  */
 class ShowAdminController extends AbstractController
 {
     /**
      * Lists all the user's shows.
      */
-    public function cgetAction(Request $request)
+    public function cgetAction(Request $request, AclProvider $aclProvider, EntityManagerInterface $entityManager)
     {
-        $this->get('camdram.security.acl.helper')->ensureGranted('ROLE_USER');
-
-        $ids = $this->get('camdram.security.acl.provider')->getEntitiesByUser($this->getUser(), '\\Acts\\CamdramBundle\\Entity\\Show');
-        $shows = $this->getDoctrine()->getRepository('ActsCamdramBundle:Show')->findIdsByDate($ids);
+        $ids = $aclProvider->getEntitiesByUser($this->getUser(), '\\Acts\\CamdramBundle\\Entity\\Show');
+        $shows = $entityManager->getRepository('ActsCamdramBundle:Show')->findIdsByDate($ids);
 
         return $this->render('show_admin/index.html.twig', array('shows' => $shows));
     }
@@ -33,10 +36,9 @@ class ShowAdminController extends AbstractController
     /**
      * Lists all the shows waiting for approval by this user.
      */
-    public function cgetUnauthorisedAction(Request $request)
+    public function cgetUnauthorisedAction(Request $request, ModerationManager $moderationManager)
     {
-        $this->get('camdram.security.acl.helper')->ensureGranted('ROLE_USER');
-        $shows = $this->get('acts.camdram.moderation_manager')->getEntitiesToModerate();
+        $shows = $moderationManager->getEntitiesToModerate();
 
         return $this->render('show_admin/unauthorised.html.twig', array('shows' => $shows));
     }
